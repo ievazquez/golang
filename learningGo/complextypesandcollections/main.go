b3bdc220c7dafc9817272248c10af368e008c292
Iván Vázquez <ievazquez@hotmail.com>
2022-12-21 20:24:22 +0000

    Handling arrays

---
+++ learningGo/complextypesandcollections/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var colors [3]string
+
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+
+	fmt.Println(colors)
+	fmt.Println(" number of colors ", len(colors))
+
+
+	var numbers = [5] int { 1, 2 ,3 ,4 ,5}
+	fmt.Println(numbers)
+}
